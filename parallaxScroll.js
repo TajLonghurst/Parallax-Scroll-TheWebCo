d131b221705cdbd50ed3e962071c530b292ce60e
Tajinboxx <TajLonghurstsoft@gmail.com>
2023-06-01 21:24:22 +1200

    Optimization

--- parallaxScroll.js
+++ parallaxScroll.js
@@ -30,7 +30,7 @@ const updateImage = (index) => {
   context.drawImage(img, 0, 0);
 };
 
-this.addEventListener("DOMContentLoaded", preloadImages, true);
+// this.addEventListener("DOMContentLoaded", preloadImages, true);
 
 window.addEventListener("scroll", () => {
   const scrollTop = html.scrollTop;
@@ -45,5 +45,3 @@ window.addEventListener("scroll", () => {
     requestAnimationFrame(() => updateImage(frameIndex + 1));
   }
 });
-
-// preloadImages();
